912fa0a0c743a11738f9b18048d08ee835ba234a
Dylan Nguyen <a1869735@adelaide.edu.au>
2024-08-07 01:16:00 +0930

    First try

--- even.c
+++ even.c
@@ -2,3 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+
+void handle_sighup(int signum) {
+    printf("Ouch!\n");
+}
+
+void handle_sigint(int signum) {
+    printf("Yeah!\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <number_of_even_numbers>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    if (n <= 0) {
+        fprintf(stderr, "The number of even numbers should be a positive integer.\n");
+        return 1;
+    }
+    
+    signal(SIGHUP, handle_sighup);
+    signal(SIGINT, handle_sigint);
+
+    for (int i = 0; i < n; ++i) {
+        printf("%d\n", i * 2);
+        sleep(5);
+    }
+
+    return 0;
+}
